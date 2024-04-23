bf1280e537213005a07efd47d9944c0b0d4ad98b
wyliebrown1990 <57993915+wyliebrown1990@users.noreply.github.com>
2024-04-23 15:23:09 -0400

    adding frontend service

--- order-service/app.py
+++ order-service/app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-from flask import Flask, jsonify
+import random
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 
 # Dummy data
-orders = {
-    1: {"user_id": 1, "product_ids": [1, 2]},
-    2: {"user_id": 2, "product_ids": [2]}
-}
+orders = {}
+order_id = 1  # to keep track of the next order ID
 
 @app.route('/orders/<int:order_id>')
 def get_order(order_id):
@@ -15,6 +15,25 @@ def get_order(order_id):
         return jsonify(order)
     else:
         return jsonify({"error": "Order not found"}), 404
+
+@app.route('/purchase', methods=['POST'])
+def purchase_product():
+    global order_id
+    user_id = request.json.get('user_id')
+    product_id = request.json.get('product_id')
+    price = random.randint(1, 20)  # generate a random price between $1 and $20
+
+    # Creating a new order
+    new_order = {
+        "user_id": user_id,
+        "product_ids": [product_id],
+        "price": price
+    }
+    orders[order_id] = new_order
+    response = jsonify(new_order)
+    response.status_code = 201
+    order_id += 1
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5003, debug=True)
